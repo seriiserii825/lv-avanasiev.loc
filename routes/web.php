245e1827a9977197bed6b55b284ce62735a495cd
serii <seriiburduja@gmail.com>
2022-01-08 15:11:41 +0200

    post controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'blog'], function(){
+    Route::resource('posts', '\App\Http\Controllers\Blog\PostController')->names('blog.posts');
+});
