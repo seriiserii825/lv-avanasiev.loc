b6d1ec830077ab846b702628b8d31fe9b0dda051
serii <seriiburduja@gmail.com>
2022-01-14 17:52:45 +0200

    flase errors

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class CategoryController extends Controller
 {
@@ -39,15 +41,24 @@ class CategoryController extends Controller
 
     public function update(Request $request, $id)
     {
+        $rules = [
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+            'description' => 'nullable|min:6|max:500',
+            'parent_id' => 'nullable|integer|exists:blog_categories,id'
+        ];
+        $request->validate($rules);
+
+
         $item = BlogCategory::find($id);
         if (empty($item)) {
-            return back()->withErrors(["msg" => "not found category with id ${id}"])->withInput();
+            return back()->with(["error" => "not found category with id ${id}"])->withInput();
         }
         $data = $request->all();
         try {
             $item->fill($data)->save();
             return redirect()->route('admin_categories.edit', $id)->with(['success' => 'Success save data']);
-        }catch (QueryException $exception) {
+        } catch (QueryException $exception) {
             return back()->with(['error' => $exception->getMessage()]);
         }
     }
