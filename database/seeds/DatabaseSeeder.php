7d1847e475fb0d528332c72464a1699f5cbd51ce
serii <seriiburduja@gmail.com>
2022-01-21 22:02:51 +0200

    feat: delete blog post migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
-        $this->call(BlogCategorySeeder::class);
-        factory(BlogPost::class, 100)->create();
+//        $this->call(UserTableSeeder::class);
+//        factory(BlogPost::class, 1)->create();
+//        $this->call(BlogCategorySeeder::class);
     }
 }
